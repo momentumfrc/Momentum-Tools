3f5f96f53614ee259bbb87ea953e9629cd3b3c9e
Jordan Powers <jordanpowers1227@gmail.com>
2019-07-18 00:33:06 -0700

    Remove NetworkTables from BrightnessFilter
    That should be handled by the robot code, not by the library

--- neopixel-leds/src/main/java/org/usfirst/frc/team4999/lights/BrightnessFilter.java
+++ neopixel-leds/src/main/java/org/usfirst/frc/team4999/lights/BrightnessFilter.java
@@ -1,33 +1,8 @@
 package org.usfirst.frc.team4999.lights;
 
-import edu.wpi.first.networktables.TableEntryListener;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.networktables.NetworkTableEntry;
-
-
 public class BrightnessFilter {
 	
 	private static double brightness = 0.4;
-	
-	private static BrightnessFilter instance;
-	
-	private final String key = "BRIGHTNESS";
-	
-	private NetworkTableEntry entry;
-	
-	private BrightnessFilter() {
-		try {
-			entry = SmartDashboard.getEntry(key);
-			entry.setNumber(brightness);
-			
-			entry.addListener((notification) -> {
-				brightness = truncate(notification.value.getDouble());
-			}, TableEntryListener.kUpdate|TableEntryListener.kImmediate);
-		} catch (java.lang.NoClassDefFoundError e) {
-			System.out.println("No networktables!");
-		}
-		
-	}
 	
 	public static void setBrightness(double brightness) {
 		BrightnessFilter.brightness = truncate(brightness);
@@ -41,20 +16,11 @@ public class BrightnessFilter {
 		return in;
 	}
 	
-	/**
-	 * Registers the brightness class to listen to changes in the robot preferences
-	 */
-	public static void register() {
-		if(instance == null)
-			instance = new BrightnessFilter();
-	}
-	
 	public static int dimValue(int value) {
 		return (int)(value * brightness);
 	}
 	
 	public static Color dimColor(Color in) {
-		register();
 		return new Color(dimValue(in.getRed()), dimValue(in.getGreen()), dimValue(in.getBlue()));
 	}
 
